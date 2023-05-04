3c127ea6403c89a4c92340a0e4450a78c52e1dcc
Abhi <abhij2116@gmail.com>
2023-05-04 12:02:01 +0530

    Retry and Circuitbreaker code completed.

--- UserService/src/main/java/com/abhi/user/service/controller/UserController.java
+++ UserService/src/main/java/com/abhi/user/service/controller/UserController.java
@@ -2,6 +2,10 @@ package com.abhi.user.service.controller;
 
 import com.abhi.user.service.entity.User;
 import com.abhi.user.service.servicelayer.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +20,10 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    int retryCount=1;
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
         User saveUser = userService.saveUser(user);
@@ -23,9 +31,25 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    //@CircuitBreaker(name="ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUser(@PathVariable String userId){
+        logger.info("No. of retry counts: {}", retryCount);
+        retryCount++;
         User user=userService.getUser(userId);
         return ResponseEntity.status(HttpStatus.FOUND).body(user);
+    }
+
+    //creating fallback method for CircuitBreaker
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex){
+        logger.info("Fallback is executed because service is down : {}",ex.getMessage());
+        User user=User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("Dummy User is created because service is down.")
+                .userID("1234")
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @GetMapping()
